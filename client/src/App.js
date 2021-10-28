0ff5c300d45e59cb65325d930a4b155e7b1fe90e
Phil Huynh <philiphuynh98@gmail.com>
2021-10-28 13:22:20 -0400

    Edited state property names

---
+++ client/src/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import products from '../../sampleData/products/productList.js';
+import productInfo from '../../sampleData/products/productInfo.js';
+import productStyles from '../../sampleData/products/productStyles.js';
+import reviewSample from '../...sampleData/reviews/reviewSample.js';
+import reviewMeta from '../...sampleData/reviews/reviewMeta.js';
+import qaSample from '../../sampleData/qaSample.js';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      currentProduct: null,
+      products: products,
+      productInfo: productInfo,
+      productStyles: productStyles,
+      reviewData: reviewSample,
+      reviewMeta: reviewMeta,
+      qaData: qaSample
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <nav>
+          <span>Logo</span>
+          <span>Search</span>
+        </nav>
+        <div>
+          Overview Component
+        </div>
+        <div>
+          Q/A Component
+        </div>
+        <div>
+          Ratings and Reviews Component
+        </div>
+      </div>
+    )
+  }
+}
