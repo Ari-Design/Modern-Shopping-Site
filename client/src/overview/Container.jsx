6e3b910319377f4de40c9f819fe44e3806dbf556
cmccurd <cam.alex.mccurdy@gmail.com>
2021-10-28 19:27:47 -0400

    added Gallery component and appended an img

--- client/src/overview/Container.jsx
+++ client/src/overview/Container.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import Gallery from './Gallery.jsx';
 
 const Container = () => {
 
   return (<div className="overview_container" >
             <div className="whitespace_left"></div>
-            <div ></div>
-            <div ></div>
+            <div className="gallery_container"><Gallery /></div>
+            <div className="styles_container"></div>
+            <div className="productInfo_container"></div>
             <div className="whitespace_right"></div>
           </div>)
 };
