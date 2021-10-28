8f9a5632fa82bf8f57e69872c05e8f6bfb5675fa
Phil Huynh <philiphuynh98@gmail.com>
2021-10-28 13:48:41 -0400

    fixed one more import error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import products from '../../sampleData/products/productList.js';
 import productInfo from '../../sampleData/products/productInfo.js';
 import productStyles from '../../sampleData/products/productStyle.js';
 import reviewSample from '../../sampleData/reviews/reviewSample.js';
-import reviewMeta from '../../sampleData/reviews/reviewMeta.js';
+import reviewMeta from '../../sampleData/reviews/reviewsMeta.js';
 import qaSample from '../../sampleData/qaSample.js';
 
 class App extends React.Component {
