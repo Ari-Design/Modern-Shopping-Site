87777cdd64644095d74a4e35679fbd4ccf9a1f60
cmccurd <cam.alex.mccurdy@gmail.com>
2021-10-29 15:53:05 -0400

    finished the css for the img gallery

--- client/src/overview/Gallery.jsx
+++ client/src/overview/Gallery.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Carousel from './Carousel.jsx';
 import left_arrow from '../../../dist/assets/images/left_arrow.png';
 import right_arrow from '../../../dist/assets/images/right_arrow.png';
+import full_screen_icon from '../../../dist/assets/images/full-screen-icon.png';
+import down_arrow from '../../../dist/assets/images/down_arrow.png';
 
 const styles = {
   arrowLeft: {
@@ -17,6 +19,20 @@ const styles = {
     position: 'absolute',
     top: '302.75px',
     left: '94%'
+  },
+  fullscreen: {
+    width: 30,
+    margin: 'auto',
+    position: 'absolute',
+    top: '4%',
+    left: '93%'
+  },
+  arrowDown: {
+    width: 20,
+    margin: 'auto',
+    position: 'absolute',
+    top: '450px',
+    left: '6.85%'
   },
   media: {
     height: '100%',
@@ -40,6 +56,8 @@ class Gallery extends React.Component {
         <Carousel />
         <img style={styles.arrowLeft} src={left_arrow}></img>
         <img style={styles.arrowRight} src={right_arrow}></img>
+        <img style={styles.fullscreen} src={full_screen_icon}></img>
+        <img style={styles.arrowDown} src={down_arrow}></img>
         <img style={styles.media} src="https://images.unsplash.com/photo-1549831243-a69a0b3d39e0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2775&q=80"></img>
       </React.Fragment>
     );
