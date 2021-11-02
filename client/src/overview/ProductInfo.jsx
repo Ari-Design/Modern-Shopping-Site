0c31b0936e0cea1c9fc8a00c9f9d12c12bc1f1b4
cmccurd <cam.alex.mccurdy@gmail.com>
2021-11-02 19:59:58 -0400

    styling and dynamic rendering for the style components

---
+++ client/src/overview/ProductInfo.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+class ProductInfo extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <>
+      <h3>slogan</h3>
+      <p>description</p>
+      <div>extra description stuff</div>
+      </>
+    )
+  }
+};
+
+export default ProductInfo;
