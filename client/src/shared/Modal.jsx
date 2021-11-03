8015dbf16b776aa89729652bbe7d3fef3cf09e05
cmccurd <cam.alex.mccurdy@gmail.com>
2021-11-03 16:42:47 -0400

    finished the fullscreen portion of the modal

--- client/src/shared/Modal.jsx
+++ client/src/shared/Modal.jsx
@@ -1,50 +1,53 @@
 import React from 'react';
 
+const close = (e, onClose) => {
+  e.preventDefault()
 
-class Modal extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isOpen: true,
-    };
-    this.close = this.close.bind(this);
+  if (onClose) {
+    onClose()
+  }
+};
+
+const styles = {
+  media: {
+    height: '100%',
+    width: '100%',
+    objectFit: 'scale-down',
+  }
+};
+
+const Modal = (props) => {
+
+  if (props.fullscreen === false) {
+    return null;
   }
 
-  close() {
-    this.setState({ isOpen: false })
+  let modalStyle = {
+    position: 'fixed',
+    height: '100%',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    zIndex: '9999',
+    background: '#fff'
   }
 
-  render() {
-    if (this.state.isOpen === false) {
-      return null;
-    }
-
-    let modalStyle = {
-      position: 'absolute',
-      top: '50%',
-      left: '50%',
-      transform: 'translate(-50%, -50%)',
-      zIndex: '9999',
-      background: '#fff'
-    }
-
-    let backdropStyle = {
-      position: 'absolute',
-      width: '100%',
-      height: '100%',
-      top: '0px',
-      left: '0px',
-      zIndex: '9998',
-      background: 'rgba(0, 0, 0, 0.3)'
-    }
-
-    return (
-      <div>
-        <div style={modalStyle}>{this.props.children}</div>
-        <div style={backdropStyle} onClick={this.close} />
-      </div>
-    )
+  let backdropStyle = {
+    position: 'fixed',
+    width: '100%',
+    height: '100%',
+    top: '0px',
+    left: '0px',
+    zIndex: '9998',
+    background: 'rgba(0, 0, 0, .8)'
   }
+
+  return (
+    <div>
+      <div style={modalStyle}><img style={styles.media} src={props.currentImg.url}></img></div>
+      <div style={backdropStyle} onClick={e => close(e, props.onClose)} />
+    </div>
+  )
 };
 
 export default Modal;
