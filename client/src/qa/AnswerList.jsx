196cd27f87a1d685c453207d9534432252f52797
Phil Huynh <philiphuynh98@gmail.com>
2021-11-03 13:03:00 -0400

    Added put request to main main app component and server.Refactored QA container to better support question search.

--- client/src/qa/AnswerList.jsx
+++ client/src/qa/AnswerList.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import Answer from './Answer.jsx';
 
 
-var AnswerList = ({ answers }) => (
+var AnswerList = ({ answers, handleHandR }) => (
   <div>
     {Object.keys(answers).map((key) => (
       <Answer
         key={`${key}`}
         answer={answers[key]}
+        handleHandR={handleHandR}
       />
     ))}
   </div>
