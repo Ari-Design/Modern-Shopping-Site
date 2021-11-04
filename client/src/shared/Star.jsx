c9b6ca63cf433d85959a6ec6b87d0063c8cbeee9
Alex Carlman <alexcarlman1@gmail.com>
2021-11-04 10:03:13 -0400

    Add clarifying comment

--- client/src/shared/Star.jsx
+++ client/src/shared/Star.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+// As of 11/04/21, only renders whole stars (but will accept fractional numbers). To be updated.
 var Star = ({numStars}) => {
   var mapArr = [1, 2, 3, 4, 5];
   return mapArr.map((star, index) => {
