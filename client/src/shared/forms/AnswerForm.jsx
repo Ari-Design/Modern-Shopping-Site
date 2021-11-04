91dd38ae081c7f7b85fde36d06367c102fa98546
Phil Huynh <philiphuynh98@gmail.com>
2021-11-04 13:09:53 -0400

    Udated CSS for Submit Answer form

--- client/src/shared/forms/AnswerForm.jsx
+++ client/src/shared/forms/AnswerForm.jsx
@@ -36,47 +36,49 @@ class AnswerForm extends React.Component {
 
   render() {
     return (
-      <div>
-        <form className="answer_form"onSubmit={this.handleSubmit}>
-          <label className="a_email_label">
-            Email:&nbsp;&nbsp;
-            <input
-              className="a_email_input"
-              type="email"
-              name="email"
-              placeholder="Example: jack@email.com"
-              value={this.state.email}
-              onChange={this.handleChange}
-              required
-            />
-          </label>
-          <label className="a_nickname_label">
-            Nickname:&nbsp;&nbsp;
-            <input
-              className="a_nickname_input"
-              type="text"
-              name="nickname"
-              placeholder="Example: jack543!"
-              value={this.state.nickname}
-              onChange={this.handleChange}
-              required
-            />
-          </label>
-          <label className="a_input_label">
-            Answer:
-            <textarea
-              className="answer_input"
-              cols="75"
-              rows="14"
-              name="answer"
-              value={this.state.question}
-              onChange={this.handleChange}>
-            </textarea>
-          </label>
-          <button className="a_cancel">Cancel</button>
-          <input className="answer_submit" type="submit" value="Submit" />
-        </form>
-      </div>
+      <div className="answer_form">
+      <form onSubmit={this.handleSubmit}>
+        <h2 className="answer_form_header">Submit Your Answer</h2>
+        <h3 className="answer-form_subheader">Product - Question</h3>
+        <label className="a_email_label">
+          Email:&nbsp;&nbsp;
+          <input
+            className="a_email_input"
+            type="email"
+            maxLength="60"
+            placeholder="Why did you like the product or not?"
+            name="email"
+            value={this.state.email}
+            onChange={this.handleChange}
+          />
+        </label>
+        <label className="a_nickname_label">
+          Nickname:&nbsp;&nbsp;
+          <input
+            className="a_nickname_input"
+            type="text"
+            maxLength="60"
+            placeholder="Example: jackson11!"
+            name="nickname"
+            value={this.state.nickname}
+            onChange={this.handleChange}
+          />
+        </label>
+        <label className="a_input_label">
+          Question:
+          <textarea
+            className="answer_input"
+            cols="75"
+            rows="14"
+            name="answer"
+            value={this.state.question}
+            onChange={this.handleChange}>
+          </textarea>
+        </label>
+        <button className="a_cancel">Cancel</button>
+        <input className="answer_submit" type="submit" value="Submit" />
+      </form>
+    </div>
     )
   }
 }
