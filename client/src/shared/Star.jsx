76dd66f1843ba07233194e67ed9f78da4a5f030d
Alex Carlman <alexcarlman1@gmail.com>
2021-11-05 11:14:21 -0400

    Import average star rating to Star shared component. To render average rating, pass prop avgRating={true}.

--- client/src/shared/Star.jsx
+++ client/src/shared/Star.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import {averageRating} from '../Reviews/RatingsBreakdown.jsx';
 // As of 11/04/21, only renders whole stars (but will accept fractional numbers). To be updated.
-var Star = ({numStars}) => {
+var Star = ({numStars, avgRating}) => {
   var mapArr = [1, 2, 3, 4, 5];
-
+  if (avgRating) {
+    numStars = averageRating;
+  }
   return mapArr.map((star, index) => {
     if (numStars - 1 >= index) {
       return (
