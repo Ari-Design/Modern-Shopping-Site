5a788ff8288a6392ee54d5bbe0f0f158c5adf348
Alex Carlman <alexcarlman1@gmail.com>
2021-11-07 17:12:42 -0500

    Fix factor labeling

--- client/src/Reviews/Factors.jsx
+++ client/src/Reviews/Factors.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 var Factors = ({metaReviews}) => {
   var characteristics = metaReviews.characteristics;
-  return ['Size', 'Width', 'Comfort', 'Quality', 'Length', 'Fit'].map((factor) => {
-    if (characteristics[factor]) {
-      var markerPosition = JSON.stringify(characteristics[factor].value * 20) + '%';
+  return [['Size', 'Small', 'Perfect', 'Big'], ['Width', 'Narrow', 'Perfect', 'Wide'], ['Comfort', 'Not Comfortable', '', 'Perfect'], ['Quality', 'Low Quality', '', 'Perfect'], ['Length', 'Too Short', 'Perfect', 'Too Long'], ['Fit', 'Too Small', 'Perfect', 'Too Big']].map((factor) => {
+    if (characteristics[factor[0]]) {
+      var markerPosition = JSON.stringify(characteristics[factor[0]].value * 20) + '%';
       return (
-        <div key={characteristics[factor].id}>
-          <span>{factor}</span>
+        <div key={characteristics[factor[0]].id}>
+          <span>{factor[0]}</span>
           <svg width='100%' height='24px' style={{display: 'inline-block'}}>
             <g>
             <rect fill='#ebebeb' width='27%' height='8px' x='0%'></rect>
-            <text x='0%' y='95%' fontSize='10'>Too small</text>
+            <text x='0%' y='95%' fontSize='10'>{factor[1]}</text>
             <rect fill='#ebebeb' width='27%' height='8px'  x='33%'></rect>
-            <text x='33%' y='95%' fontSize='10'>Perfect</text>
+            <text x='33%' y='95%' fontSize='10'>{factor[2]}</text>
             <rect fill='#ebebeb' width='27%' height='8px'  x='66%'></rect>
-            <text x='66%' y='95%' fontSize='10'>Too large</text>
+            <text x='66%' y='95%' fontSize='10'>{factor[3]}</text>
             <rect fill='#000000' width='5%' height='8px' x={markerPosition}></rect>
             </g>
           </svg>
