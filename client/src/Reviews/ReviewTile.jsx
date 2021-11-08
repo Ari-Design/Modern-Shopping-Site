8d5f09c6439d1c0f8f2b8fc1a62b47b8c3053951
Alex Carlman <alexcarlman1@gmail.com>
2021-11-07 20:46:23 -0500

    Stylin'

--- client/src/Reviews/ReviewTile.jsx
+++ client/src/Reviews/ReviewTile.jsx
@@ -6,10 +6,10 @@ var ReviewTile = ({review}) => {
   return (
     <>
     <div className="review_stars"><h4><Star numStars={review.rating}/></h4></div>
-    <div className="review_reviewer"><h5>(conditional verified check) {review.reviewer_name}, {moment(review.date).format("MMMM Do, YYYY")}</h5></div>
+    <div className="review_reviewer"><h5>&#9745; {review.reviewer_name}, {moment(review.date).format("MMMM Do, YYYY")}</h5></div>
     <div className="review_summary"><h4>{review.summary}</h4></div>
     <div className="review_body">{review.body}</div>
-    {review.recommend ? <div className="review_recommend"><i>&#10003; I recommend this product</i></div> : null}
+    {review.recommend ? <div className="review_recommend"><br/><i>&#10003; I recommend this product</i></div> : null}
     <br/>
     <div className="review_footer">
     Helpful? <a id="helpful_button" className="helpful_button"><u>Yes</u> ({review.helpfulness})  |  </a>
