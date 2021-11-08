90cd76f274e3b3e1c4318e8b8929fe2c1fc53769
Alex Carlman <alexcarlman1@gmail.com>
2021-11-07 21:17:15 -0500

    Text alignment

--- client/src/Reviews/ReviewTile.jsx
+++ client/src/Reviews/ReviewTile.jsx
@@ -5,9 +5,12 @@ import moment from 'moment';
 var ReviewTile = ({review}) => {
   return (
     <>
-    <div className="review_stars"><h4><Star numStars={review.rating}/></h4></div>
-    <div className="review_reviewer"><h5>&#9745; {review.reviewer_name}, {moment(review.date).format("MMMM Do, YYYY")}</h5></div>
-    <div className="review_summary"><h4>{review.summary}</h4></div>
+    <div className="stars_and_reviewers">
+    <div className="review_stars" style={{float:'left'}}><h4><Star numStars={review.rating}/></h4></div>
+    <div className="review_reviewer" style={{float:'right'}}><h5>&#9745; {review.reviewer_name}, {moment(review.date).format("MMMM Do, YYYY")}</h5></div>
+    </div>
+    <br/>
+    <span className="review_summary"><br/><h4><br/>{review.summary}</h4></span>
     <div className="review_body">{review.body}</div>
     {review.recommend ? <div className="review_recommend"><br/><i>&#10003; I recommend this product</i></div> : null}
     <br/>
