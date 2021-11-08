936fb6365833786b84a059278bf6e9c4747b2768
Alex Carlman <alexcarlman1@gmail.com>
2021-11-07 21:27:31 -0500

    Bookmark spacing on ReviewTile.jsx

--- client/src/Reviews/ReviewTile.jsx
+++ client/src/Reviews/ReviewTile.jsx
@@ -5,7 +5,7 @@ import moment from 'moment';
 var ReviewTile = ({review}) => {
   return (
     <>
-    <span className="review_stars" style={{float:'left'}} y='100%'><Star numStars={review.rating}/></span>
+    <span className="review_stars" style={{float:'left'}}><Star numStars={review.rating}/></span>
     <span className="review_reviewer" style={{float:'right'}}>&#9745; {review.reviewer_name}, {moment(review.date).format("MMMM Do, YYYY")}</span>
     <br/>
     <span className="review_summary"><h4>{review.summary}</h4></span>
