6c17216b04c564a595b91162738c194540ebf1c0
Alex Carlman <alexcarlman1@gmail.com>
2021-11-11 17:48:36 -0500

    Review form

--- client/src/Reviews/AddCharacteristics.jsx
+++ client/src/Reviews/AddCharacteristics.jsx
@@ -1,15 +1,36 @@
 import React from 'react';
 
-var AddCharacteristics = ({metaReviews}) => {
-  console.log(metaReviews);
-  var characteristics = metaReviews.characteristics;
+class AddCharacteristics extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      size: '',
+      comfort: '',
+      quality: '',
+      length: '',
+      fit: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.id] : e.target.value
+    })
+  }
+  render () {
   return [['Size', 'Small', 'Perfect', 'Big'], ['Width', 'Narrow', 'Perfect', 'Wide'], ['Comfort', 'Uncomfortable', '', 'Perfect'], ['Quality', 'Low', '', 'High'], ['Length', 'Too Short', 'Perfect', 'Too Long'], ['Fit', 'Too Small', 'Perfect', 'Too Big']].map((factor) => {
+    var characteristics = this.props.metaReviews.characteristics;
+    if (characteristics[factor[0]]) {
     return (
-      <div>
+      <div key={characteristics[factor[0]].id}>
         <label id={factor[0]}>{factor[0]}</label>
-        <input type="range" id={factor[0]} min="0" max="5" value="2.5" step="1"></input>
+        <input type="range" id={factor[0].toLowerCase()} className="factor_range" min="0" max="5" step="1" onChange={this.handleChange}></input>
       </div>
-    )});
+    )
+    }
+  });
+  }
 }
 
 export default AddCharacteristics;
--- client/src/shared/forms/AddReviewForm.jsx
+++ client/src/shared/forms/AddReviewForm.jsx
@@ -65,7 +65,7 @@ class AddReviewForm extends React.Component {
           <label className="recommend_label">
             Would You Recommend This Product? <input
               className="recommend"
-              type="radio"
+              type="checkbox"
               >
             </input>
           </label>
@@ -77,6 +77,7 @@ class AddReviewForm extends React.Component {
               type="email"
               maxLength="60"
               placeholder="For authentication reasons, you will not be emailed"
+              size="60"
               value={this.state.email}
               onChange={this.handleChange}
             />
@@ -101,6 +102,7 @@ class AddReviewForm extends React.Component {
               className="reviewSummary"
               type="text"
               maxLength="60"
+              size="60"
               placeholder="Example: Best purchase ever!"
               value={this.state.summary}
               onChange={this.handleChange}
