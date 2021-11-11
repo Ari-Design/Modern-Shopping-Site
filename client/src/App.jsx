4c6c7ba24a87cd0b18a9f95f4b2b42eb9dcb1136
Phil Huynh <philiphuynh98@gmail.com>
2021-11-11 13:12:30 -0500

    qa component CSS update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -140,7 +140,7 @@ class App extends React.Component {
     return (
       <div>
         <header>
-          <nav>
+          <nav className="page_top">
             <span>Logo</span>
             <span>Search</span>
             <span className="product_search">
